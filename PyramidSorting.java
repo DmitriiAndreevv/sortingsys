1cf27f43b69ff86a0d483962f86c1bba6df2b71b
dmitriy.andreev.1995@inbox.ru <dmitriy.andreev.1995@inbox.ru>
2023-06-20 20:33:34 +0300

    Initial commit

--- PyramidSorting.java
+++ PyramidSorting.java
@@ -1,60 +1,58 @@
-
-
 public class PyramidSorting {
 
     public static void main(String[] args) {
-        int[] array = new int[]{5,4,3,2,1,2,3};
+        int[] array = new int[]{5, 4, 3, 2, 1, 2};
+        //heapifysort(array,10,1);
         heapSort(array);
-        //heapifysort(array);
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) {
             System.out.print(array[i] + " ");
+
 
         }
     }
-    public static int heapifysort(int[] array){
 
-        int i = 0;
-        int left =  2 * i + 1;
+    public static void heapifysort(int[] array, int heapsize, int i) {
+
+        int left = 2 * i + 1;
         int right = 2 * i + 2;
         int max = i;
 
         // сравниваем left через if
-        if (left < array.length ){
-            if (array[left] > array[max]){
-                max =  left;
+        if (left < heapsize) {
+            if (array[left] > array[max]) {
+                max = left;
             }
             //сравниваем right
-            if (right < array.length){
-                if (array[right] > array[max]){
-                   max = right;
+            if (right < heapsize) {
+                if (array[right] > array[max]) {
+                    max = right;
                 }
                 //если произойдет замена
-                if (max != i){
+                if (max != i) {
                     int temp = array[i];
+                    array[i] = array[max];
                     array[max] = temp;
-                    heapifysort(array);
+
+                    heapifysort(array, heapsize, i);
                 }
             }
         }
-        return left;
     }
-    public static void heapSort(int[]array){
+
+    public static void heapSort(int[] array) {
         if (array.length == 0)
             return;
         //начинаем строить кучу
         int length = array.length;
         // проходим от первого без ответвлений к корню
-        for (int i = length / 2 -1 ; i > 0; i--) {
-            heapifysort(array);
+        for (int i = array.length / 2 - 1; i >= 0; i--) ;
 
-        for (i = length - 1; i >=0 ; i--) {
+
+        for (int i = array.length - 1; i >= 0; i--) {
             int temp = array[0];
             array[0] = array[i];
             array[i] = temp;
-
-            heapifysort(array);
-            }
-
+            heapifysort(array, i, 0);
         }
     }
 }
